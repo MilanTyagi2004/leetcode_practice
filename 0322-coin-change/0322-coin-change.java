c6f86aac5321a86f5592836c9bf5b3270495302a
MilanTyagi2004 <157273416+MilanTyagi2004@users.noreply.github.com>
2025-06-11 12:09:39 +0530

    Time: 14 ms (87.49%), Space: 44.8 MB (47.26%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,8 +1,8 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
         int[] dp = new int[amount + 1];
-        for (int i = 0; i <= amount; i++) {
-            dp[i] = amount + 1;
+        for(int i=0;i<=amount;i++){
+            dp[i] = amount+1;
         }
         dp[0] = 0;
 
